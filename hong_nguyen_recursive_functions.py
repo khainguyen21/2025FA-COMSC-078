41ed3e4fdadf3eb54d9f80032d91627b4ee948a4
Khai Nguyen <khainguyen2004@gmail.com>
2025-09-18 13:14:35 -0700

    Completed Recursive Function Assignment

---
+++ hong_nguyen_recursive_functions.py
@@ -0,0 +1,37 @@
+
+
+def display_em(lower, upper):
+    """ This recursive function displays the consecutive integers
+    from its lower to its upper bounds """
+    if lower == upper + 1:
+        return 0
+
+    print(lower)
+    display_em(lower + 1, upper)
+    return None
+
+
+def add_em(lower, upper):
+    """ This recursive function calculates the sum of the consecutive
+    integers from its lower to its upper bounds"""
+    if lower == upper + 1:
+        return 0
+
+    return lower + add_em(lower + 1, upper)
+
+
+def applyToEach(f, lower_bound, upper_bound):
+    """ This higher-order function applies the included function
+    to its lower and upper bound arguments"""
+    return f(lower_bound, upper_bound)
+
+lower = int(input("Enter a lower bound: "))
+upper = int(input("Enter a upper bound: "))
+
+print()
+print("The consecutive integers:")
+applyToEach(display_em, lower, upper)
+
+print()
+total = applyToEach(add_em, lower, upper)
+print(f"Add up to {total}")
